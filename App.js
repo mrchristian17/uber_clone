b38e032bd3b7d10e7e8f464abeb3cebdfa0d8e0c
mrchristian17 <dcwrath17@gmail.com>
2022-09-01 16:06:27 -0600

    Set up google place autocomplete to give autofill suggestions for inputted location

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+
 export default function App() {
   const Stack = createNativeStackNavigator();
 
