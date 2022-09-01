b32054be9e7413f7cbb63d37462ac5d293b1cb4e
mrchristian17 <dcwrath17@gmail.com>
2022-09-01 11:41:29 -0600

    Added home screen component with uber logo and tailwind classnames for styles

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
-
-// Setup Redux
 
 export default function App() {
   return (
     <Provider store ={store}>
-      <View style={styles.container}>
-        <Text>Let's build uber</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
