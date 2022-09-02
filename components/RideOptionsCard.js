c3875867c149ef22178c9f761f4758309c52e452
mrchristian17 <dcwrath17@gmail.com>
2022-09-02 15:01:10 -0600

    Added generalized prices for different fake uber vehicles ex: uber x, xl, lux and distance and travel time information.  Also added a menu button to go back from map screen

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -4,13 +4,15 @@ import {
   Text,
   TouchableOpacity,
   View,
-  Image,
+  Image
 } from "react-native";
 import React, { useState } from "react";
 import tw from "twrnc";
-import { Icon } from "@rneui/base";
+import { Icon} from "@rneui/base";
 import { useNavigation } from "@react-navigation/native";
 import SafeAreaView from "react-native-safe-area-view";
+import { selectTravelTimeInformation } from "../slices/navSlice";
+import { useSelector } from "react-redux";
 
 const data = [
   {
@@ -33,10 +35,15 @@ const data = [
   },
 ];
 
+// IF there is a surplus of ride requests, a premium rate is charged
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
   const navigation = useNavigation();
-  const [selected, setSelected] = useState(null);
-
+  const [selected,setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
+  
+  
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
       <View style={tw`border-t border-gray-200`}>
@@ -46,7 +53,9 @@ const RideOptionsCard = () => {
         >
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
-        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride - {
+          travelTimeInformation?.distance?.text
+        }</Text>
       </View>
 
       <FlatList
@@ -54,35 +63,40 @@ const RideOptionsCard = () => {
         keyExtractor={(item) => item.id}
         renderItem={({ item: { id, title, multiplier, image }, item }) => (
           <TouchableOpacity
-            onPress={() => setSelected(item)}
+          onPress={() => setSelected(item)}
             style={tw`flex-row justify-between items-center px-10 ${
               id === selected?.id && "bg-gray-200"
             }`}
           >
-            <Image
-              style={{
-                width: 100,
-                height: 100,
-                resizeMode: "contain",
-              }}
-              source={{ uri: image }}
-            />
-            <View style={tw`-ml-6`}>
-              <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>4 passengers</Text>
-            </View>
-            <Text style={tw`text-xl`}>$99</Text>
+              <Image
+                style={{
+                  width: 100,
+                  height: 100,
+                  resizeMode: "contain",
+                }}
+                source= {{uri:image}}
+              />
+              <View style={tw`-ml-6`}>
+                <Text style={tw`text-xl font-semibold`}>{title}</Text>
+                <Text>{travelTimeInformation?.duration?.text} Travel Time</Text>
+              </View>
+              <Text style={tw`text-xl`}>
+                {new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(
+                  (travelTimeInformation?.duration.value *
+                    SURGE_CHARGE_RATE *
+                    multiplier) /
+                    100
+                )}
+              </Text>
           </TouchableOpacity>
         )}
       />
-      <View>
-        <TouchableOpacity
-          disabled={!selected}
-          style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
-        >
-          <Text style={tw`text-center text-white text-xl`}>
-            Choose {selected?.title}
-          </Text>
+      <View style={tw`mt-auto border-t border-gray-200`}>
+        <TouchableOpacity disabled={!selected} style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}>
+          <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import Map from "../components/Map";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import NavigateCard from "../components/NavigateCard";
 import RideOptionsCard from "../components/RideOptionsCard";
+import { Icon } from "@rneui/base";
+import { useNavigation } from "@react-navigation/native";
 
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
   return (
     <View>
+      <TouchableOpacity 
+      onPress = {() => navigation.navigate("HomeScreen")}
+      style={tw`absolute top-16 left-8 z-50 bg-gray-100 rounded-full shadow-lg p-3`}>
+        <Icon name="menu" type="fontawesome" />
+      </TouchableOpacity>
+
       <View style={tw`h-1/2`}>
         <Map />
       </View>
